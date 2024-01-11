c63d3ede02406a150713144eaef7765ce51d2360
EC2 Default User <ec2-user@ip-172-31-19-56.ap-southeast-2.compute.internal>
2024-01-11 12:33:45 +0000

    function1

---
+++ app.js
@@ -0,0 +1,50 @@
+// let hello = 'Hello World';
+// alert(hello);
+
+// let int1 = 1;
+// let int2 = 2;
+// alert(int1);
+
+// let orange = 120;
+// let apple = 120;
+
+// if文
+// if (orange < apple) {
+//   alert('みかん < リンゴ');
+// }else if (orange == apple) {
+//   alert('みかん = リンゴ');
+// }else {
+//   alert('みかん > リンゴ');
+// }
+
+// // while文
+// let max = 100;
+// let num = 1;
+// let count = 0;
+
+// while(num < max){
+//   num = num * 2;
+//   count = count + 1;
+// }
+// alert('2をかけ続けて' + max + 'を超えるのに必要だったのは' + count + '回です');
+
+// do while文
+
+// let max = 100;
+// let num = 1;
+// let count = 0;
+
+// do {
+//   num = num * 2;
+//   count = count + 1;
+// } while (num < max);
+// alert('2をかけ続けて' + max + 'を超えるのに必要だったのは' + count + '回です');
+
+// for文
+let i;
+let num = 0;
+
+for (i = 1; i < 11; i++) {
+  num = num + i;
+}
+alert('1から10まで足した結果は' + num + 'です');
